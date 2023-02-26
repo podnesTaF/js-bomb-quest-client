f79a48e0ae2b17e0aa1caa750745973fd95721bc
podnes <podnestrade@gmail.com>
2023-02-26 13:36:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'js-quest',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
